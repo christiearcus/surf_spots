840e562db94ffb5a50c0ff325c3b22f6616afdcd
Christie <christie.arcus@gmail.com>
2019-11-26 19:55:58 +1100

    Rename app bundle

--- client/bundles/App/startup/App.js
+++ client/bundles/App/startup/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import SearchResults from '../components/SearchResults';
 
 const App = (props) => (
-    <SearchResults {...props} />
+    <SearchResults spots={JSON.parse(props.spots)} />
 );
 
 export default App;
